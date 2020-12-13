3c7374cffd8b721e2e35c24716a5ad1c50eb7668
whitejamer <mihailkuzin.job@yandex.ru>
2020-12-14 00:40:33 +0300

    Complete functional

---
+++ src/components/ErrorHelper.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="input__error-message">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ErrorHelper',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import Maska from "maska"
+import Vuelidate from "vuelidate"
 
 Vue.config.productionTip = false
+
+Vue.use(Maska)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app")
